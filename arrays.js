91dcbedd21e7a37de823f40c055bc207a0206388
sumit <sumitmessi018@gmail.com>
2023-10-12 17:22:08 +0530

    arrays

---
+++ arrays.js
@@ -0,0 +1,21 @@
+let marks_class_12 = [94, 53 ,85, 84, false, "not present" ,]
+console.log(marks_class_12)
+
+marks_class_12[6] = 95//adding a new valuus to the array
+console.log(marks_class_12[0])
+console.log(marks_class_12[1])
+console.log(marks_class_12[2])
+console.log(marks_class_12[3])
+console.log(marks_class_12[4])
+console.log(marks_class_12[5])
+console.log(marks_class_12[6])
+console.log(marks_class_12[7])
+
+
+console.log("the length of marks_class_12 is " , marks_class_12.length)
+marks_class_12[0] = 99//changing the value of an array
+console.log(marks_class_12)
+
+console.log(typeof marks_class_12)
+marks_class_12[7] =76 //adding another new value to the array
+
